c68e51e7d302ad1e744f4f62b070e08dbab1de09
Dan Versalov <DanV@v1021-wn-88-177.campus-dynamic.uwaterloo.ca>
2017-09-15 23:25:59 -0400

    Initial Commit

---
+++ Cloutfit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cloutfit
+//
+//  Created by Dan Versalov on 9/15/17.
+//  Copyright © 2017 Dan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
